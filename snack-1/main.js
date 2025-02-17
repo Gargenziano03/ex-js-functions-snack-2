8a834f1cbbc0bf0a6b08ac8a210a851aae7f7e80
Gargenziano03 <gargenziano03@gmail.com>
2025-02-17 10:08:02 +0100

    snack 1.1

---
+++ snack-1/main.js
@@ -0,0 +1,7 @@
+//1 Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma
+// dichiarazione funzione
+function somma(a, b) {
+    return a + b
+}
+// invocazione
+console.log(somma(3, 7));
