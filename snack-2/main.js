2897436b049b237cf23fc53bbe18902107402464
Gargenziano03 <gargenziano03@gmail.com>
2025-02-17 10:52:18 +0100

    snack2

---
+++ snack-2/main.js
@@ -0,0 +1,5 @@
+//Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+// dichiarazione funzione
+function quadrato(a) { return a * a }
+// invocazione
+console.log(quadrato(2));
